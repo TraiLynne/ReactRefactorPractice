4bca7abf39ff645b1735da2e47bdc421a7354e37
Trai Lynne Compton <trailynne@gmail.com>
2024-10-14 17:53:37 -0600

    Refactored Click Counter into Functional Component - Utilized `useState` hook

--- app/src/components/ClickCounter/FunctionalComponent.js
+++ app/src/components/ClickCounter/FunctionalComponent.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const ReClickCounter = () => {
-	return <p>Click Counter Functional Component</p>;
+	// State
+	const [count, setCount] = useState(0);
+
+	// Event Handler
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<h1>Button Clicked: {count} times</h1>
+			<button onClick={handleClick}>Click Me</button>
+		</div>
+	);
 };
 
 export default ReClickCounter;
