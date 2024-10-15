9ca329fe1e209c89d58364cdebe6ed32093ff022
Trai Lynne Compton <trailynne@gmail.com>
2024-10-14 19:33:25 -0600

    Refactored `Weather` into Functional Component

--- app/src/components/Weather/FunctionalComponent.js
+++ app/src/components/Weather/FunctionalComponent.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 const ReWeather = () => {
-	return <p>Weather Functional Component</p>;
+	const [temp, setTemp] = useState(null);
+
+	useEffect(() => {
+		console.log("Fetching Data");
+		setTimeout(() => {
+			console.log("Done");
+			setTemp(78);
+		}, 3000);
+	}, []);
+
+	return (
+		<div>
+			<h1>Current Temperature: {temp ? `${temp}°F` : "Loading..."}</h1>
+		</div>
+	);
 };
 
 export default ReWeather;
