fa914a77c869c7975267f58cad0ab8b9d9eae84c
Trai Lynne Compton <trailynne@gmail.com>
2024-10-14 18:35:34 -0600

    Refactored `Form` into Functional Component

--- app/src/components/Form/FunctionalComponent.js
+++ app/src/components/Form/FunctionalComponent.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 const ReForm = () => {
-	return <p>Form Functional Component</p>;
+	// State
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+
+	const handleNameChange = (e) => {
+		setName(e.target.value);
+	};
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value);
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.table({ name, email });
+	};
+
+	return (
+		<form onSubmit={(e) => handleSubmit(e)}>
+			<input
+				type="text"
+				name="name"
+				value={name}
+				onChange={(e) => handleNameChange(e)}
+				placeholder="Name"
+			/>
+			<input
+				type="email"
+				name="email"
+				value={email}
+				onChange={(e) => handleEmailChange(e)}
+				placeholder="Email"
+			/>
+			<button type="submit">Submit</button>
+		</form>
+	);
 };
 
 export default ReForm;
