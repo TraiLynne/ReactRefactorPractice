68cc8169fa247331279ad106773c07704858190c
Trai Lynne Compton <trailynne@gmail.com>
2024-10-14 19:19:57 -0600

    Refactored `UserProfile` into Functional Component - Added a timer in there to complete the effect.

--- app/src/components/UserProfile/FunctionalComponent.js
+++ app/src/components/UserProfile/FunctionalComponent.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 const ReUserProfile = () => {
-	return <p>User Profile Functional Component</p>;
+	const [user, setUser] = useState(null);
+	const userData = { name: "John Doe", age: 30 };
+
+	useEffect(() => {
+		console.log("Fetching Data");
+		setTimeout(() => {
+			console.log("Done");
+			setUser(userData);
+		}, 3000);
+	}, []);
+
+	return (
+		<div>
+			{user ? (
+				<div>
+					<h1>{user.name}</h1>
+					<p>Age: {user.age}</p>
+				</div>
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
+	);
 };
 
 export default ReUserProfile;
+
+// I wasn't really seeing the effect so I added a timer
