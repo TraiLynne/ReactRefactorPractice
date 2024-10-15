2c388b83f21ad1b7ae3f046411365a03750f6d18
Trai Lynne Compton <trailynne@gmail.com>
2024-10-14 18:40:43 -0600

    Refactored `ListDisplay` into Functional Component

--- app/src/components/ListDisplay/FunctionalComponent.js
+++ app/src/components/ListDisplay/FunctionalComponent.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 const ReListDisplay = () => {
-	return <p>List Display Functional Component</p>;
+	// State
+	const [items, setItems] = useState(["Item 1", "Item 2", "Item 3"]);
+
+	return (
+		<ul>
+			{items.map((item, index) => (
+				<li key={index}>{item}</li>
+			))}
+		</ul>
+	);
 };
 
 export default ReListDisplay;
