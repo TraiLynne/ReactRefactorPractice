f81c04b97f74df1629ae054532cf374f7de7d489
Trai Lynne Compton <trailynne@gmail.com>
2024-10-14 18:23:49 -0600

    Refactored `Counter` into Functional Component

--- app/src/components/Counter/FunctionalComponent.js
+++ app/src/components/Counter/FunctionalComponent.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const ReCounter = () => {
-	return <p>Counter Functional Component</p>;
+	// State
+	const [count, setCount] = useState(0);
+
+	return (
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={() => setCount(count + 1)}>Increment</button>
+			<button onClick={() => setCount(count - 1)}>Decrement</button>
+		</div>
+	);
 };
 
 export default ReCounter;
+
+// I learned my lesson in that `ClickCounter` and also in the `ColorPicker` to avoid that rerender
